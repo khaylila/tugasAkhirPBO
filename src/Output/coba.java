525436ae52894c88d91d4055dbe89a92c2f5e56f
Mochamad Roiyan <mochamadroiyan@gmail.com>
2023-11-09 19:58:43 +0700

    commit from laptop

---
+++ src/Output/coba.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Output;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ *
+ * @author milea
+ */
+public class coba {
+    public static void main(String[] args) {
+//        LocalDateTime myDateObj = LocalDateTime.now();
+//        System.out.println(myDateObj);
+//        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        System.out.println(myDateObj.format(myFormatObj));
+//        Date asdf = new Date();
+//        asdf
+//        System.out.println(String.valueOf(asdf.));
+    }
+}
